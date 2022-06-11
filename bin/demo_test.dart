b83241717c3aeadf9606b7a4b6fd7364e9b87179
nvtphong-Executionlab <phong.nguyen@executionlab.asia>
2022-06-11 10:43:29 +0700

    first commit

---
+++ bin/demo_test.dart
@@ -0,0 +1,5 @@
+import 'package:demo_test/demo_test.dart' as demo_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${demo_test.calculate()}!');
+}
---
+++ test/demo_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:demo_test/demo_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
